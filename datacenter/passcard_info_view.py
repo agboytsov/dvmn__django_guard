8a6f52b8a28170b3ee37212e98be71ceebe78172
Alexander <agboytsov@gmail.com>
2024-01-14 10:54:35 +0300

    Task done

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,18 +1,41 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils.timezone import localtime
+
+
+def get_duration(visit):
+    started = localtime(visit.entered_at)
+    if not visit.leaved_at:
+        stopped = localtime()
+    else:
+        stopped = visit.leaved_at
+    delta = stopped - started
+    return delta
+
+
+def format_duration(delta):
+    return f'{int(delta.total_seconds()/60/60)}:' \
+           f'{int((delta.total_seconds()/60)%60)}:'\
+           f'{delta.seconds%60:00}'
+
+
+def check_suspicious(visit, amount=60):
+    spent = get_duration(visit)
+    if amount * 60 < spent.total_seconds():
+        return True
+    return False
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
+    passcard = Passcard.objects.filter(passcode=passcode)
+    get_object_or_404(passcard)
     # Программируем здесь
-
+    all_last_visits = Visit.objects.filter(passcard=passcard[0])
     this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
+        {'entered_at' : visit.entered_at,
+         'duration':format_duration(get_duration(visit)),
+         'is_strange':check_suspicious(visit)} for visit in all_last_visits
     ]
     context = {
         'passcard': passcard,
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,27 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
 
 
 def storage_information_view(request):
     # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    non_closed_visits = []
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
+    for visitor in active_visits:
+        local = localtime(visitor.entered_at)
+        now = localtime()
+        delta = now - local
+        non_closed_visits.append(
+            {
+            'who_entered': visitor.passcard,
+            'entered_at': visitor.entered_at,
+            'duration': f'{int(delta.total_seconds() / 60 / 60)}:'
+                        f'{int((delta.total_seconds() / 60) % 60)}:'
+                        f'{delta.seconds % 60:00}',
+            }
+        )
+        pass
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
